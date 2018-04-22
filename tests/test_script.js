351ec5ac70b79615339b80574e4df8eaa178a174
noncomputable <andcomputable@gmail.com>
2018-04-21 23:43:45 -0400

    changed samples to tests, incorporated agent-making features

--- tests/test_script.js
+++ tests/test_script.js
@@ -1,15 +1,14 @@
-poly1 = poly2 = [];
-
 let bounding_box = [[40.6469, -73.5255], [40.6390, -73.5183]];
 
-let sample_map = L.map("sample_map").fitBounds(bounding_box).setZoom(16);
+let map = L.map("sample_map").fitBounds(bounding_box).setZoom(16);
 L.tileLayer(
 	"http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
 	{
 		attribution: "Thanks to <a href=\"http://openstreetmap.org\">OpenStreetMap</a> community",
 		maxZoom: 18,
 	}
-).addTo(sample_map);
+).addTo(map);
 
-let sample_agentmap = new Agentmap(sample_map);
-sample_agentmap.mapify(bounding_box, sample_data);
+let agentmap = new Agentmap(map);
+agentmap.mapify(bounding_box, sample_data);
+agentmap.agentify(100, agentmap.seqUnitAgentMaker);
