35b9fc48aa1f5d1efc603f70d5423e72b51a3b18
noncomputable <andcomputable@gmail.com>
2018-08-25 17:47:06 -0400

    removed practically redundant exception throw

--- src/index.js
+++ src/index.js
@@ -3,8 +3,4 @@ agents = require('./agents'),
 buildings = require('./buildings'),
 utils = require('./utils');
 
-if (typeof(L) === "undefined") {
-	throw "L is undefined! Make sure that Leaflet.js is loaded.";
-}
-
 L.A = Object.assign({}, agentmap, agents, buildings, utils);
