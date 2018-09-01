96b04ac050801fd1b4d2e80369d53cbca27765bf
noncomputable <andcomputable@gmail.com>
2018-09-01 03:36:25 -0400

    added babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,19 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, "site/dist"),
 		filename: "agentmaps.js"
-	}
+	},
+	module: {
+		rules: [
+		    {
+		      test: /\.js$/,
+		      exclude: /(node_modules|bower_components)/,
+		      use: {
+			loader: 'babel-loader',
+			options: {
+			  presets: ['@babel/preset-env']
+			}
+		      }
+		    }
+		  ]
+		}
 }
