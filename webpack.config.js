f2a2094a9a6579f329944d547e696f0343273c53
Andrew Gregory Tsesis <andcomputable@gmail.com>
2024-09-17 01:30:26 -0400

    webpack conf formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
 /* This file is part of AgentMaps and is released under the Simplified BSD License. */
 
-const path = require('path');
- 
+const path = require("path");
+
 module.exports = {
-	devtool: "eval",
-	output: {
-		path: path.resolve(__dirname, "site/dist"),
-		filename: "agentmaps.js"
-	},
-	module: {
-		rules: [
-		    {
-		      test: /\.js$/,
-		      exclude: /(node_modules|bower_components)/,
-		      use: {
-			loader: 'babel-loader',
-			options: {
-			  presets: ['@babel/preset-env']
-			}
-		      }
-		    }
-		  ]
-		}
-}
+  devtool: "eval",
+  output: {
+    path: path.resolve(__dirname, "site/dist"),
+    filename: "agentmaps.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
